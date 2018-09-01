e234ca59321164096c4a569bb37798d6170e42ca
Joshua Ciccone-Okodo <joshokodo@gmail.com>
2018-09-01 09:33:40 -0600

    final version complete

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #### Make change project
+
+#### Instructions: 
+####   1.(feel free to try to break the program by entering ANY invalid input)
+####   2. first enter the price of the goods being bought
+####   3. then enter the amount you are paying with
+####   4. sit back and watch the program calculate your change for you
+####   5. have a nice day
--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -13,21 +13,28 @@ public class MakeChange {
 		double tender = 0.0;
 
 		System.out.println("Welcome to the Money Sink! " 
-							+ "Where we don't judge you for blowing all that moola, Baby!");
+					  + "Where we don't judge you for blowing all that moola, Baby!");
 
+		// gets price of goods from user
 		price = getMoneyInput("Enter the total price of the goods being purchased: ");
 
+		// gets amount user is paying with
 		tender = getMoneyInput("Enter the total amount you are paying with: ");
 
-		while (tender < price) {
+		// loops until the tender amount is greater or equal to the amount due
+		while (!(tender >= price)) {
 
 			System.out.println("Sorry, thats not enough money. Please try again");
 			tender = getMoneyInput("Enter the total amount you are paying with: ");
 
 		}
+		// prints change
 		printChange(price, tender);
+		return;
 
 	}
+
+	// methods
 
 	// gets user input for currency values and error checks
 	public static double getMoneyInput(String prompt) {
@@ -70,10 +77,10 @@ public class MakeChange {
 	public static void printChange(double price, double tender) {
 
 		double change = tender - price; // gets change
+		int bills = (int) change; // gets change in dollars
 
-		int changeDollars = (int) change;
-
-		double changeCoins = change - (double) ((int) change);
+		double tempCoins = change - bills; // gets change in coins
+		int coins = (int) Math.round((tempCoins * 100));// rounds coins for precision and makes them ints
 
 		System.out.println("****************************************");
 		System.out.println("*                                      *");
@@ -84,76 +91,32 @@ public class MakeChange {
 		System.out.println("*                                      *");
 		System.out.printf("*   Change back:    $ %6.2f%11s*%n", change, "");
 		System.out.println("*                                      *");
-		System.out.print(getDollarChangeStrings(changeDollars));
-		System.out.print(getCoinChangeString(changeCoins));
+		bills = printChangeString("$20 bills :", bills, 20);
+		bills = printChangeString("$10 bills :", bills, 10);
+		bills = printChangeString("$5 bills :", bills, 5);
+		bills = printChangeString("$1 bills :", bills, 1);
+		coins = printChangeString("Quarters :", coins, 25);
+		coins = printChangeString("Dimes :", coins, 10);
+		coins = printChangeString("Nickels :", coins, 5);
+		coins = printChangeString("Pennies :", coins, 1);
+		System.out.println("*                                      *");
+		System.out.println("*     Thank you, Have a great Day!     *");
 		System.out.println("*                                      *");
 		System.out.println("****************************************");
+
 	}
 
-	public static String getDollarChangeStrings(int dollars) {
-		String change = "";
+	// takes in change, finds number of multiples of a specific bill or coin
+	// if there are multiples, prints out how many. then returns the remaining
+	// change.
+	public static int printChangeString(String changeType, int change, int multiple) {
 
-		int remainingDollars = dollars;
+		int numOfMultiple = change / multiple;
 
-		int numOf20s = remainingDollars / 20;
-		remainingDollars %= 20;
-
-		int numOf10s = remainingDollars / 10;
-		remainingDollars %= 10;
-
-		int numOf5s = remainingDollars / 5;
-		remainingDollars %= 5;
-
-		int numOf1s = remainingDollars / 1;
-		remainingDollars %= 1;
-
-		if (numOf20s > 0) {
-			change += String.format("*   Number of $20 bills:      %3d      *%n", numOf20s);
+		if (numOfMultiple > 0) {
+			System.out.printf("*   Number of %-16s%3d      *%n", changeType, numOfMultiple);
 		}
-		if (numOf10s > 0) {
-			change += String.format("*   Number of $10 bills:      %3d      *%n", numOf10s);
-		}
-		if (numOf5s > 0) {
-			change += String.format("*   Number of $5 bills:       %3d      *%n", numOf5s);
-		}
-		if (numOf1s > 0) {
-			change += String.format("*   Number of $1 bills:       %3d      *%n", numOf1s);
-		}
-
-		return change;
-	}
-
-	public static String getCoinChangeString(double coins) {
-		String change = "";
-
-		int remainingCoins = (int) (coins * 100);
-
-		int numOfQuarters = remainingCoins / 25;
-		remainingCoins %= 25;
-
-		int numOfDimes = remainingCoins / 10;
-		remainingCoins %= 10;
-
-		int numOfNickels = remainingCoins / 05;
-		remainingCoins %= 05;
-
-		int numOfPennies = remainingCoins / 01;
-		remainingCoins %= 01;
-
-		if (numOfQuarters > 0) {
-			change += String.format("*   Number of Quarters:       %3d      *%n", numOfQuarters);
-		}
-		if (numOfDimes > 0) {
-			change += String.format("*   Number of Dimes:          %3d      *%n", numOfDimes);
-		}
-		if (numOfNickels > 0) {
-			change += String.format("*   Number of Nickels:        %3d      *%n", numOfNickels);
-		}
-		if (numOfPennies > 0) {
-			change += String.format("*   Number of Pennies:        %3d      *%n", numOfPennies);
-		}
-
-		return change;
+		return change % multiple;
 	}
 
 }
